b8e5f72cd4820be8f0700bbc3eafa73e271dafb8
Shun Kimura <tokinari.pp@gmail.com>
2025-02-07 08:22:33 +0900

    retry

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: '/', // ビルド時のパス設定（Vercelで正しく動作させる）
+  base: './', // ビルド時のパス設定（Vercelで正しく動作させる）
   plugins: [react()],
   resolve: {
     alias: {
