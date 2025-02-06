cb2d265cf243b019fd8f7eaabb51be0c2c9abf36
Shun Kimura <tokinari.pp@gmail.com>
2025-02-07 07:41:42 +0900

    retry

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import path from 'path-browserify';
+import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
-      src: path.resolve(__dirname, './src'),
+      src: path.resolve(__dirname, 'src'),
     },
   },
   server: {
     host: '0.0.0.0', // ローカルネットワーク上のデバイスからアクセス可能
     port: 5173,
+  },
+  build: {
+    outDir: 'dist', // Vercel で `dist/` を正しく使うための設定
   },
 });
