c3e3c1cb3fe90f3dc35d4fba7e72aea9c6105dc5
Shun Kimura <tokinari.pp@gmail.com>
2025-02-07 15:26:03 +0900

    retry

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // `src` のエイリアスを `@` に変更（よく使われる形）
+      src: path.resolve(__dirname, './src'),
     },
   },
   server: {
